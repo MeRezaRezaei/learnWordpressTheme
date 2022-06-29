14ea81fa2d54535b3116c2e84b3c7faaa3f4d5ce
merezarezaei <merezarezaei@gmail.com>
2022-06-29 11:20:59 +0430

    init

---
+++ functions.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Twenty Twenty-Two functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Two
+ * @since Twenty Twenty-Two 1.0
+ */
+
+
+if ( ! function_exists( 'hello_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function hello_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'hello_support' );
+
+if ( ! function_exists( 'hello_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function hello_styles() {
+		// Register theme stylesheet.
+		$theme_version = wp_get_theme()->get( 'Version' );
+
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
+		wp_register_style(
+			'hello-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			$version_string
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'hello-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'hello_styles' );
+
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
